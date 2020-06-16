1df7df782457793fcb95ae88661137a1e267c0d0
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-16 22:26:25 +0100

    Added helper class 'boat_possible_moves'

---
+++ lib/boat_possible_moves.rb
@@ -0,0 +1,56 @@
+require 'coordinates'
+
+class BoatPossibleMoves
+  def self.check(coordinate, coordinates)
+    @coordinate  = coordinate
+    @coordinates = coordinates
+    possible_moves.include?(coordinate)
+  end
+
+  def self.setup
+    @moves = []
+
+    @letters = Coordinates.letters
+    @last_letter_i = @letters.index(@coordinates.keys.last[0])
+
+    @numbers = Coordinates.numbers
+    @last_number_i = @numbers.index(@coordinates.keys.last[1])
+  end
+
+  def self.possible_moves
+    return Coordinates.create if @coordinates.values.length == 0
+
+    setup
+
+    if @coordinates.values.length == 1
+      horizontal_moves
+      vertical_moves
+    end
+
+    if @coordinates.values.length > 1
+      horizontal ? horizontal_moves : vertical_moves
+    end
+
+    @moves
+  end
+
+  def self.horizontal
+    @coordinates.keys[0][0] == @coordinates.keys[1][0]
+  end
+
+  def self.horizontal_moves
+    if @numbers[@last_number_i + 1]
+      @moves << @letters[@last_letter_i] + @numbers[@last_number_i + 1]
+    elsif (@last_number_i - 1) > 0
+      @moves << @letters[@last_letter_i] + @numbers[@last_number_i - 1]
+    end
+  end
+
+  def self.vertical_moves
+    if @letters[@last_letter_i + 1]
+      @moves << @letters[@last_letter_i + 1] + @numbers[@last_number_i]
+    elsif (@last_letter_i - 1) > 0
+      @moves << @letters[@last_letter_i - 1] + @numbers[@last_number_i]
+    end
+  end
+end
